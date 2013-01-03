2ca9c055d2a9fee87eec8013f49aa8bbd91372a8
Rita <rserra16@gmail.com>
2013-01-03 11:14:26 +0000

    codigo relativo aos socios

---
+++ Source/socios.c
@@ -0,0 +1,55 @@
+#define Mnome[50]
+#define Mmorada[100]
+typedef struct
+{
+ char nome[Mnome];
+ char morada [Mmorada]
+ int num_soc
+ int dt_nasc 
+ int contacto 
+ int estado
+}SOCIO;
+/// funcao para mostrar a lista de socios
+void ver_lista()
+{
+ }
+/// funcao para adicionar socio
+/// \param nome do socio
+/// \param morada 
+/// \param numero de socio
+/// \param data de nascimento
+/// \param contacto
+int adicionar(char nome[Mnome], char morada [Mmorada], int num_soc, int dt_nasc, int contacto, int estado);
+{
+}
+/// funcao para modificar a ficha de socio
+/// \param nome do socio
+/// \param morada 
+/// \param numero de socio
+/// \param data de nascimento
+/// \param contacto
+int modificar (char nome[Mnome],char morada [Mmorada],int num_soc,int dt_nasc,int contacto,int estado);
+{
+}
+/// funcao para remover socio
+void remover()
+{
+ }
+/// funcao para gravar no ficheiro
+void gravar_ficheiro()
+{
+ }
+/// funcao para ler do ficheiro
+void ler_ficheiro()
+{
+ }
+/// funcao que mostra um menu para fazer cada uma das operaçoes: ver, adicionar, remover, gravar, mostrar
+///\param ?
+int menu_socios()
+{
+    
+}
+ 
+
+
+        
