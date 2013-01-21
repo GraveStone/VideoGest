886e0db9c5eb5fdd5a9626073c4b157cded3db72
MartaSantos <kitty-marta@hotmail.com>
2013-01-21 21:46:21 +0000

    funcao ver filme

--- Source/Filmes.c
+++ Source/Filmes.c
@@ -69,6 +69,69 @@ void inserir_filme()
     fclose(fp_fil);
 }
 
+/// Função para ver filmes
+
+
+void ver_filmes(void)
+ {
+  FILE *fp_ver_fil;
+  int teste,n_reg;
+
+  system("cls");
+  fp_ver_fil=fopen("filmes.txt","rb");
+  if(!fp_ver_fil)
+   {
+    printf("\n\t Erro na leitura do Ficheiro!!");
+    getch();
+    return;
+   }
+  fseek(fp_ver_fil,0L,SEEK_END);
+  n_reg=(ftell(fp_ver_fil))/sizeof(Filme);
+  rewind(fp_ver_filme);
+  if(n_reg==0)
+   {
+    printf("\n Ficheiro vazio");
+    getch();
+    fclose(fp_ver_fil);
+    return;
+   }
+  printf("\nN§ filme\tSocio");
+  do
+   {
+    teste=fread(&aux_al,sizeof(Filme),1,fp_ver_fil);
+    if(teste==1)
+     {
+      printf("\n%d\t%d\t%ld-%d-%d\t%d n_reg %d",aux_al.num_filme,aux_al.num_socio,aux_al.data_lev.ano, aux_al.data_lev.mes, aux_al.data_lev.dia,aux_al.estado,n_reg);
+     }
+   }
+  while(!feof(fp_ver_fil));
+  getch();
+  fclose(fp_ver_fil);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /// Função para modificar filme.
 
 int modificar_filme()
@@ -100,35 +163,6 @@ void carregar_filmes()
 {}
 
 
-int filme_existe(long int n_filme)
-{
-  FILE *fp_fil;
-  int teste;
-
-  system("cls");
-  fp_fil=fopen("filmes.txt","rb");
-  if(!fp_fil)
-   {
-    printf("\n\t Erro na leitura do Ficheiro!!");
-    getch();
-    return 0;
-   }
-   do
-   {
-        teste=fread(&aux_fil,sizeof(filme),1,fp_fil);
-        if(teste==1)
-        {
-            if(aux_fil.num_filme==n_filme)
-            {
-               fclose(fp_fil);
-               return 1;
-            }
-        }
-    }
-    while(!feof(fp_fil));
-    getch();
-    fclose(fp_fil);
-    return 0;
-}
+int
 
 
